a21a6045fc9244c9ffb579c8168c7814a6e94cb1
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2023-02-27 18:22:14 +0800

    与SpringBoot项目计算不一致

--- stock-and-fund/js/stockAndFund.js
+++ stock-and-fund/js/stockAndFund.js
@@ -16,7 +16,7 @@ let URL = {
     // 下面这两个地址可以通过本地启动stock-and-fund项目
     // github地址为：https://github.com/bu6030/stock-and-fund
     GET_FUND_FROM_LOCAL_SERVICE : "http://127.0.0.1:8080/chrome/fund",
-    GET_STOCK_AND_FUND_FROM_LOCAL_SERVICE : "http://1.117.221.208:8080/chrome/stockAndFund",
+    GET_STOCK_AND_FUND_FROM_LOCAL_SERVICE : "http://127.0.0.1:8080/chrome/stockAndFund",
 }
 
 var appList = [
