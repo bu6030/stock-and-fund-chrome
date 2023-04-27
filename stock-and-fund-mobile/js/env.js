9b8debcbb621ab4303473a14ab2cc753cf2aefd1
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2023-04-27 09:35:02 +0800

    增加手机浏览器版本

--- stock-and-fund-mobile/js/env.js
+++ stock-and-fund-mobile/js/env.js
@@ -3,8 +3,8 @@ let Env = {
     GET_FUND_FROM_TIANTIANJIJIN: "http://fundgz.1234567.com.cn/js/",
     // 下面这两个地址可以通过本地启动stock-and-fund项目
     // github地址为：https://github.com/bu6030/stock-and-fund
-    GET_FUND_FROM_LOCAL_SERVICE: "http://1.117.221.208:8080/chrome/fund",
-    GET_STOCK_AND_FUND_FROM_LOCAL_SERVICE: "http://1.117.221.208:8080/chrome/stockAndFund",
+    GET_FUND_FROM_LOCAL_SERVICE: "http://127.0.0.1:8080/chrome/fund",
+    GET_STOCK_AND_FUND_FROM_LOCAL_SERVICE: "http://127.0.0.1:8080/chrome/stockAndFund",
 }
 
 let develop = false;
