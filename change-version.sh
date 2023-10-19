e446faba3b88dd26f980cf1068c2eb8a275ffa95
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2023-10-19 13:20:15 +0800

    版本更新代码 sh

--- change-version.sh
+++ change-version.sh
@@ -16,6 +16,7 @@ sed -i "" 's/'$CURRENT_V'/'$NEW_V'/g' ./stock-and-fund/full-screen.html
 echo ========GIT 提交文件========
 git add ./stock-and-fund/manifest.json
 git add ./stock-and-fund/popup.html
+git add ./stock-and-fund/full-screen.html
 git commit -m "修改版本号"$NEW_V
 git push
 
