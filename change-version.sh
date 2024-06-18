b5b36ae96e0c0ff24315d368c0f400e9fedaafbf
Xuesong.Bu <Xuesong.Bu@homecredit.cn>
2024-06-18 16:15:38 +0800

    修改脚本

--- change-version.sh
+++ change-version.sh
@@ -4,9 +4,10 @@ CURRENT_V=$(echo $line | grep '"version' ./stock-and-fund/manifest.json | awk '{
 echo ========当前版本$CURRENT_V========
 CURRENT_REL=$(echo "$CURRENT_V" | awk '{split($0,a,"."); print a[1]}')
 CURRENT_INCR=$(echo "$CURRENT_V" | awk '{split($0,a,"."); print a[2]}')
-CURRENT_FIX=$(echo "$CURRENT_V" | awk '{split($0,a,"."); print a[3]}')
+CURRENT_MINOR=$(echo "$CURRENT_V" | awk '{split($0,a,"."); print a[3]}')
+CURRENT_FIX=$(echo "$CURRENT_V" | awk '{split($0,a,"."); print a[4]}')
 NEW_FIX=$(($CURRENT_FIX+1))
-NEW_V=$CURRENT_REL.$CURRENT_INCR.$NEW_FIX
+NEW_V=$CURRENT_REL.$CURRENT_INCR.$CURRENT_MINOR.$NEW_FIX
 echo ========新版本$NEW_V========
 echo ========替换文件========
 sed -i "" 's/'$CURRENT_V'/'$NEW_V'/g' ./stock-and-fund/manifest.json
