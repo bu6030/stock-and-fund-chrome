09ef59df13452f96e5aabcde699335ccf4fc4e69
buxuesong <buxuesong@foxmail.com>
2024-12-31 22:13:38 +0800

    修改版本自动修改脚本

--- mini-change-version.sh
+++ mini-change-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo ========开始========
-CURRENT_V=$(echo $line | grep '"version' ./stock-and-fund/manifest.json | awk '{split($0,a,"\""); print a[4]}')
+CURRENT_V=$(echo $line | grep '"version' ./stock-and-fund-mini/manifest.json | awk '{split($0,a,"\""); print a[4]}')
 echo ========当前版本$CURRENT_V========
 CURRENT_REL=$(echo "$CURRENT_V" | awk '{split($0,a,"."); print a[1]}')
 CURRENT_INCR=$(echo "$CURRENT_V" | awk '{split($0,a,"."); print a[2]}')
@@ -25,16 +25,14 @@ fi
 NEW_V=$CURRENT_REL.$CURRENT_INCR.$CURRENT_MINOR.$NEW_FIX
 echo ========新版本$NEW_V========
 echo ========替换文件========
-sed -i "" 's/'$CURRENT_V'/'$NEW_V'/g' ./stock-and-fund/manifest.json
-sed -i "" 's/'$CURRENT_V'/'$NEW_V'/g' ./stock-and-fund/manifest-firefox.json
-sed -i "" 's/'$CURRENT_V'/'$NEW_V'/g' ./stock-and-fund/popup.html
-sed -i "" 's/'$CURRENT_V'/'$NEW_V'/g' ./stock-and-fund/full-screen.html
+sed -i "" 's/'$CURRENT_V'/'$NEW_V'/g' ./stock-and-fund-mini/manifest.json
+sed -i "" 's/'$CURRENT_V'/'$NEW_V'/g' ./stock-and-fund-mini/manifest-firefox.json
+sed -i "" 's/'$CURRENT_V'/'$NEW_V'/g' ./stock-and-fund-mini/popup.html
 
 echo ========GIT 提交文件========
-git add ./stock-and-fund/manifest.json
-git add ./stock-and-fund/manifest-firefox.json
-git add ./stock-and-fund/popup.html
-git add ./stock-and-fund/full-screen.html
+git add ./stock-and-fund-mini/manifest.json
+git add ./stock-and-fund-mini/manifest-firefox.json
+git add ./stock-and-fund-mini/popup.html
 git commit -m "修改版本号"$NEW_V
 git push
 
